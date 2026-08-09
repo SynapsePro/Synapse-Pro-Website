461006721375de97174dd681436885a6d4d8d326
SynapsePro <help.synapse.pro@gmail.com>
2026-08-10 01:29:12 +0200

    Add admin dashboard configuration

---
+++ admin/command-center-config.js
@@ -0,0 +1,8 @@
+window.SYNAPSEPRO_COMMAND_CENTER = Object.freeze({
+  supabaseUrl: "https://brsrxgwqncgibnvqjcuj.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyc3J4Z3dxbmNnaWJudnFqY3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDQyNzcsImV4cCI6MjA5NTAyMDI3N30.pNosGgjiTLizTc_xChzzkP8kuQqNDtoe2YkXP6CN0Mo",
+  dispatchFunction: "dispatch-action",
+  historicalOnboardingCount: 3000,
+  productName: "SynapsePro",
+});
+
